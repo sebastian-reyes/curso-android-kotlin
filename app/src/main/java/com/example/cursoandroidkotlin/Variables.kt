7177decdb6ba06cfefe9c3a12db5261a02712cff
sebastian-reyes <sebastianreyes2105@gmail.com>
2023-09-18 14:48:55 -0500

    funciones

--- app/src/main/java/com/example/cursoandroidkotlin/Variables.kt
+++ app/src/main/java/com/example/cursoandroidkotlin/Variables.kt
@@ -1,23 +1,19 @@
 package com.example.cursoandroidkotlin
 
+const val edad: Int = 21 //global - fuera de las funciones
+
 fun main() {
     println("Valores(Constantes) - Variables:")
+    //variablesNumericas()
+    //variablesBoolean()
+    //variablesAlfanumericas()
+    showMyAge(21)
+    showMyAge() //No se envían variables - se agarra la variable por defecto
+    showMyName("Sebastián")
+    println(resta(8,4))
+}
 
-    /**
-     * Valores Numéricas
-     */
-    //Integer
-    val edad: Int = 21
-
-    //Long - a partir de acá el uso de memoria se incrementa
-    val edadLong: Long = 21
-
-    //float
-    val floatExample: Float = 30.5f
-
-    //Double
-    val doubleExample: Double = 30.5
-
+fun variablesAlfanumericas() {
     /**
      * Valores alfanuméricas
      */
@@ -30,11 +26,6 @@ fun main() {
     //String
     val stringExample: String = "Sebastián__@//∟"
 
-    //Boolean
-    val booleanExample: Boolean = true
-    val booleanExample2: Boolean = false
-
-    println(booleanExample)
     println(stringExample)
 
     /**
@@ -46,4 +37,43 @@ fun main() {
 
     //Concatenar
     println("mi nombre es: $nombre y tengo $edad años")
+}
+
+fun variablesBoolean() {
+    //Boolean
+    val booleanExample: Boolean = true
+    val booleanExample2: Boolean = false
+    println(booleanExample)
+}
+
+fun variablesNumericas() {
+    /**
+     * Valores Numéricas
+     */
+
+    //Long - a partir de acá el uso de memoria se incrementa
+    val edadLong: Long = 21
+
+    //float
+    val floatExample: Float = 30.5f
+
+    //Double
+    val doubleExample: Double = 30.5
+}
+
+/**
+ * Se pueden declarar valores por defecto,
+ * para ser usados en caso se llame a la función y
+ * no manden los parámetros
+ */
+fun showMyAge(age: Int = 1) {
+    println("mi edad es $age")
+}
+
+fun showMyName(name: String) {
+    println("mi nombre es $name")
+}
+
+fun resta(primerNumero: Int, segundoNumero: Int): Int {
+    return primerNumero - segundoNumero
 }
