1e619d38a8f22c00afea4bdb9954ea08f2651a22
sebastian-reyes <sebastianreyes2105@gmail.com>
2023-09-19 09:42:54 -0500

    arrays

---
+++ app/src/main/java/com/example/cursoandroidkotlin/Arrays.kt
@@ -0,0 +1,26 @@
+package com.example.cursoandroidkotlin
+
+fun main() {
+    val diasSemana =
+        arrayOf("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo")
+
+    println(diasSemana[0])
+    println(diasSemana.size)
+
+    if (diasSemana.size >= 8) {
+        println(diasSemana[7])
+    } else {
+        println("No hay más valores en el array")
+    }
+
+    diasSemana[0] = "holi"
+    println(diasSemana[0])
+    println("--------------------------")
+
+    /**
+     * forEach
+     */
+    for (p in diasSemana){
+        println(p)
+    }
+}
